986a2fe38879751cb18fcace765decebd30f913c
doodiyehezkel <doodiyehezkel@gmail.com>
2016-03-28 14:16:55 +0300

    Added files via upload

--- finalEx1/src/src/ReadFile.java
+++ finalEx1/src/src/ReadFile.java
@@ -1,9 +1,8 @@
-package src;
-
 
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -11,26 +10,27 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
-
 public class ReadFile {
 
 	public static void main(String[] args)  {
 		 try {
-			
-			 FileWriter fff = new FileWriter("C:\\ex1\\te2iscszcziii.txt");
-			 BufferedWriter gggg = new BufferedWriter(fff);
-			 gggg.write(4444);
-			 FileReader a = new FileReader("C:\\ex1\\test1.txt") ;
+			 File d = new File("C:/doodi.txt");
+			 d.createNewFile();
+			 FileWriter output = new FileWriter(d);
+			 FileReader a = new FileReader("C:/test1.txt") ;
+			 FileReader c = new FileReader("C:/test1.txt") ;
 			 BufferedReader b = new BufferedReader(a);
+			 BufferedReader e = new BufferedReader(c);
+			 
 			 String [] temp ;
+			 String temp1 ;
+			 temp1 = e.readLine();
 			 int from,to,numOfBL;
 			 ArrayList<Integer> jjj = new ArrayList<>();
 			 String s = b.readLine();
 			 int numofline = Integer.parseInt(s);
-			 int i = 3 ; // start of the black point on list 
-			 // 5 4 1 0
-			 In AAAA = new In("C:\\ex1\\tinyEWD.txt"); 
+			 int i = 3 ; 
+			 In AAAA = new In("C:/tinyEWD.txt"); 
 			 Graph t = new Graph(AAAA);
 			 for (int j = 0; j < numofline ; j++) {
 				 s = b.readLine();
@@ -46,27 +46,18 @@ public class ReadFile {
 				 }
 				 t.blin(jjj);
 				 Graph_algo ddddd = new Graph_algo(t , from);
-				 System.out.println(ddddd.distTo(to));
-				 //ddddd.distTo(to);
-			 
-				 fff.write(j);
-				 
+				 ddddd.distTo(to);
+				 temp1 = e.readLine();
+				 output.write(temp1);
+				 output.write(" ");
+				 output.write(Double.toString(ddddd.distTo(to)));
+				 output.write("\n");
 			}
-			 gggg.close();
-				 
-				 
-				 
-			
+			 output.close();
 	
-			 
-			 
 		} catch (Exception e) {
 			System.out.println("ther is no such file with this name !! ");
 		}
-	
-		 
-		
-		
 	}
 
 }
